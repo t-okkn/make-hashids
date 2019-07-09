a502bb8faa9df4667435fbcba21e2c44f48c4fe9
t-okkn <kakka@mail.osaka.kakka.net>
2019-07-09 11:41:13 +0900

    [fix] Nginx Unitを使用しないように変更・VersionとRevision情報を受け取れるように変更

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "nginx/unit"
+// import "nginx/unit"
+
+var (
+	Version string
+	Revision string
+)
 
 // summary => main関数（サーバを開始します）
 /////////////////////////////////////////
 func main() {
-	unit.ListenAndServe(":8501", SetupRouter())
-	// SetupRouter().Run()
+	// unit.ListenAndServe(":8080", SetupRouter())
+	SetupRouter().Run()
 }
 
